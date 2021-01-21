63759483e91ae851dae25680b497a1278600f6fa
Rosy <rosyshrestha028@gmail.com>
2021-01-21 12:40:58 +0545

    feat: Create mock data

---
+++ src/mock/index.js
@@ -0,0 +1,102 @@
+import colors from "../constants/colors";
+
+const mockData = [
+  {
+    name: "Bhatbhateni",
+    color: colors.blue,
+    todos: [
+      {
+        title: "Jam",
+        quantity: 2,
+        completed: false,
+      },
+      {
+        title: "Ice cream",
+        quantity: 2,
+        completed: true,
+      },
+      {
+        title: "Peanut",
+        quantity: 1,
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Big mart",
+    color: colors.lightBlue,
+    todos: [
+      {
+        title: "Mandarin",
+        quantity: 2,
+        unit: "kg",
+        completed: false,
+      },
+      {
+        title: "Water melon",
+        quantity: 1,
+        unit: "kg",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "Big mart",
+    color: colors.yellow,
+    todos: [
+      {
+        title: "Mandarin",
+        quantity: 2,
+        unit: "kg",
+        completed: false,
+      },
+      {
+        title: "Water melon",
+        quantity: 1,
+        unit: "kg",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "Big mart",
+    color: colors.purple,
+    todos: [
+      {
+        title: "Mandarin",
+        quantity: 2,
+        unit: "kg",
+        completed: false,
+      },
+      {
+        title: "Water melon",
+        quantity: 1,
+        unit: "kg",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "Bhatbhateni",
+    color: colors.orange,
+    todos: [
+      {
+        title: "Jam",
+        quantity: 2,
+        completed: false,
+      },
+      {
+        title: "Ice cream",
+        quantity: 2,
+        completed: true,
+      },
+      {
+        title: "Peanut",
+        quantity: 1,
+        completed: false,
+      },
+    ],
+  },
+];
+
+export default mockData;
