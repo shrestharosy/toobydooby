0e7597df5f504fe4135231cff4e6bc993a1eb3d9
Rosy <rosyshrestha028@gmail.com>
2021-01-21 11:47:40 +0545

    feat : Create home page

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import colors from "./src/constants/colors";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ flexDirection: "row" }}>
+        <View style={styles.divider}></View>
+        <Text style={styles.title}>ToobyDooby</Text>
+        <View style={styles.divider}></View>
+      </View>
+      <View style={{ marginVertical: 48 }}>
+        <TouchableOpacity style={styles.addButton}>
+          <AntDesign name={"plus"} size={20} color={colors.blue} />
+        </TouchableOpacity>
+
+        {/* <Text style={styles.add}>Add List</Text> */}
+      </View>
     </View>
   );
 }
@@ -14,8 +26,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: colors.li,
+    paddingHorizontal: 40,
+  },
+  addButton: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  add: {
+    color: colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8,
   },
 });
