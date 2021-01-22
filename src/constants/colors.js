f1786195de3388d6d89739e3cae9160316107b8f
Rosy <rosyshrestha028@gmail.com>
2021-01-22 16:01:16 +0545

    style :  Add colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,11 +1,22 @@
-const colors = {
-  black: "#2D3436",
+export const colorPalette = {
   blue: "#24A6D9",
-  lightBlue: "#A7CBD9",
-  white: "#FFFFFF",
   yellow: "#ECD15B",
   purple: "#A977F8",
   orange: "#FCA65F",
+  middleBlueGreen: "#7DDED9",
+  fuzzyWuzzy: "#CE6664",
+  salmonPink: "#FFA3A5",
+};
+
+export const arrayOfColorPalette = Object.entries(colorPalette).map(
+  (palette) => palette[1]
+);
+
+const colors = {
+  ...colorPalette,
+  black: "#2D3436",
+  lightBlue: "#A7CBD9",
+  white: "#FFFFFF",
 };
 
 export default colors;
